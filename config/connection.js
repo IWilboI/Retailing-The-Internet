b7d53ba346e07ae01ad5ab1d4fcfd6a0994d2ec3
IWilboI <mr.wrc1998@gmail.com>
2024-09-15 22:37:18 -0600

    troubling shooting db

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PW,
+  process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'postgres',
-    port: 3000
+    port: process.env.DB_PORT ||3000,
   }
 );
 
--- models/index.js
+++ models/index.js
@@ -1,20 +1,31 @@
-// import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Category = require('./category');
+const Product = require('./product');
+const Tag = require('./tag');
+const ProductTag = require('./productTag');
 
-// Products belongsTo Category
+// Associations
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
-  Product,
   Category,
+  Product,
   Tag,
   ProductTag,
 };
+
